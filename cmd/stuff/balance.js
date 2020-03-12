d1866540b4c41b84d110de830d1184aad60481d3
Glitch (hello-express) <none>
2020-03-12 21:45:16 +0000

    💇🙉 Checkpoint ./cmd/stuff/regquest.js:3086396/480 ./cmd/stuff/balance.js:3086396/1287:12754994/1606 ./watch.json:3086396/2

---
+++ cmd/stuff/balance.js
@@ -0,0 +1,29 @@
+const { MessageEmbed } = require("discord.js")
+
+module.exports = {
+  name:"balance",
+  aliases: ["bal", "money", "cash"],
+  module:'economy',
+  usage: "optional: [member]",
+  execute: (client, message, args) => {
+    let user = message.mentions.users.first()
+    if(!user) {
+      user = message.client.users.cache.find(m => m.id == args[0] || m.username == args.join(" "))
+      if(!user) {
+        user = message.author
+      }
+    }
+    if (!client.economy.has(`${message.guild.id}.${user.id}`)) {
+      client.economy.set(message.guild.id + "." + user.id, { cash: 0, bank: 0 })
+    }
+    if (client.economy.has(`${message.guild.id}.${user.id}`)) {
+      const e = new MessageEmbed().setAuthor(user.username, user.avatarURL())
+      .setTitle(`💰 ${user.username} balance:`)
+      .addField("cash:", client.economy.get(`${message.guild.id}.${user.id}.cash`))
+      .addField("bank:", client.economy.get(`${message.guild.id}.${user.id}.bank`))
+      .addField("total:", client.economy.get(`${message.guild.id}.${user.id}.cash`) + client.economy.get(`${message.guild.id}.${user.id}.bank`))
+      message.channel.send(e)
+    }
+  }
+} 
+
--- cmd/stuff/regquest.js
+++ cmd/stuff/regquest.js
@@ -1,4 +1,12 @@
 module.exports = {
   name:"regquest",
-  cooldown: ""
+  cooldown: 600000,
+  module: "stuff",
+  execute: (client, message, args) => {
+    const chance = Math.floor(Math.random() * 101)
+    if (!client.economy.has(message.guild.id + "." + message.author.id)) client.economy.set(message.guild.id + "." + message.author.id, {cash: 0, bank: 0})
+    if (chance >= 65) {
+      client.economy.add(message.guild.id + "." + message.author.id + ".cash")
+    }
+  }
 }
