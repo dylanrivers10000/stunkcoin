57391f18e510e8704e828978787b2cb18cbf1cb9
Glitch (hello-express) <none>
2020-03-13 07:29:06 +0000

    🦂🛍 Checkpoint ./cmd/stuff/slut.js:3086396/149

--- cmd/stuff/slut.js
+++ cmd/stuff/slut.js
@@ -2,5 +2,10 @@ module.exports = {
   name:"slut",
   module: "stuff", 
   cooldown: 4140,
-  
+  execute: (client, message, args) => {
+    const chance = Math.floor(Math.random() * 101)
+    if (chance+20 <= 55) {
+      
+    }
+  }
 }
