15f71a7f8bca17a76c2bd2dd8d5952e6dffd46fd
Glitch (hello-express) <none>
2020-03-15 21:37:54 +0000

    🌓🌶 Checkpoint ./cmd/stuff/sellpanties.js:3086396/309 ./cmd/stuff/creampie.js:3086396/62

---
+++ cmd/stuff/creampie.js
@@ -0,0 +1,4 @@
+module.exports = {
+  name: "creampie",
+  cooldown: 534
+}
--- cmd/stuff/sellpanties.js
+++ cmd/stuff/sellpanties.js
@@ -1,4 +1,10 @@
 module.exports = {
   name:"sellpanties",
-  module:"stuff"
+  module:"stuff",
+  cooldown: 86400,
+  execute: (client, message, args) => {
+    const amount = Math.floor((Math.random() * 101) + 50)
+    client.economy.add(message.guild.id + '.' + message.author.id + ".cash", amount)
+    message.channel.send("You sold your worn panties earned " + amount)
+  }
 }
